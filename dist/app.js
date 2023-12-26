38b51f2923550f5c1086fbe65a33553417c99f98
dnrgus1127 <dnrgus1127@naver.com>
2023-12-27 00:02:20 +0900

    FEAT : 프록시 서버구성

---
+++ dist/app.js
@@ -0,0 +1,129 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.STALE_TIME = void 0;
+const axios_1 = require("axios");
+const { default: axios } = require("axios");
+const express = require("express");
+const app = express();
+const { fetchData, fetchPosts, postTimeUpdate, getProjectTree, getPost } = require("./src/auto_update/fetchData");
+exports.STALE_TIME = 1000 * 60 * 5;
+const cors = require("cors");
+const PROJECT_NAME_LIST = ["myport", "colorproject", "ringblog"];
+require("dotenv").config();
+let cachedata = null;
+const storage = {
+    repositoryList: {
+        data: [],
+        receivedDataTime: null,
+    },
+    readmeList: [],
+};
+// fetchData();
+// setInterval(fetchData, AUTO_FETCH_TIME);
+const corsOption = {
+    origin: ["https://dnrgus1127.github.io", "http://localhost:3000"],
+};
+app.use(cors(corsOption));
+app.use("/", (req, res, next) => {
+    const today = new Date();
+    console.log({
+        ip: req.ip,
+        time: today.getMilliseconds(),
+        uri: req.url.split("?")[0],
+        method: req.method,
+        query: req.query,
+        body: req.body,
+    });
+    next();
+});
+app.get("/", (req, res) => {
+    res.json(cachedata);
+});
+app.get("/repository", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield fetchData("https://api.github.com/users/dnrgus1127/repos", storage.repositoryList);
+        res.json(storage.repositoryList.data);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.get("/readme/:projectName", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const projectName = req.params.projectName.toLowerCase();
+        const readmeStorage = storage.readmeList.find((readme) => readme.name === projectName);
+        if (!readmeStorage) {
+            // reademe 초기화
+            storage.readmeList.push({
+                name: projectName,
+                data: [],
+                receivedDataTime: 0,
+            });
+            yield fetchData(`https://raw.githubusercontent.com/dnrgus1127/${projectName}/main/README.md`, storage.readmeList.at(-1));
+        }
+        else {
+            // 이미 요청된적 있는 경우
+            const index = storage.readmeList.findIndex((readme) => readme.name === projectName);
+            yield fetchData(`https://raw.githubusercontent.com/dnrgus1127/${projectName}/main/README.md`, storage.readmeList[index]);
+        }
+        const index = storage.readmeList.findIndex((readme) => readme.name === projectName);
+        res.send(storage.readmeList[index].data);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.get("/tree/:projectName", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const projectName = req.params.projectName;
+    try {
+        const data = yield getProjectTree(projectName);
+        res.json(data);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+// 포스트 데이터 자동 쿼리
+let postList = [];
+const updatePost = () => __awaiter(void 0, void 0, void 0, function* () {
+    const tmpPostList = yield fetchPosts();
+    postList = yield postTimeUpdate(tmpPostList);
+});
+updatePost();
+setInterval(updatePost, 1000 * 60 * 10);
+//-------------------------------------------------
+// Blog
+app.get("/postList", (req, res) => {
+    res.json(postList);
+});
+app.get("/post/:title", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const title = req.params.title;
+        const post = yield getPost(title);
+        res.send(post);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.use((err, req, res, next) => {
+    var _a;
+    if ((0, axios_1.isAxiosError)(err)) {
+        res.status(((_a = err.response) === null || _a === void 0 ? void 0 : _a.status) || 404).send();
+    }
+    else {
+        res.status(404).send("Not Found");
+    }
+});
+app.listen(process.env.PORT, () => {
+    console.log(`PORT : ${process.env.PORT}`);
+});
---
+++ dist/src/auto_update/fetchData.js
@@ -0,0 +1,64 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("../../app");
+const { default: axios } = require("axios");
+const getAxiosWithToken = (api) => {
+    return axios.get(api, {
+        headers: {
+            Authorization: `token ${process.env.TOKEN}`,
+        },
+    });
+};
+const fetchData = (api, storage) => __awaiter(void 0, void 0, void 0, function* () {
+    const now = new Date().getTime();
+    // staleTime이 만료되지 않았을 경우 return
+    if (storage.receivedDataTime !== null && now - storage.receivedDataTime <= app_1.STALE_TIME) {
+        console.log("fresh");
+        return;
+    }
+    const response = yield axios.get(`${api}`, {
+        headers: {
+            Authorization: `token ${process.env.TOKEN}`,
+        },
+    });
+    if (response.status === 200) {
+        storage.data = response.data;
+        storage.receivedDataTime = new Date().getTime();
+    }
+    else if (response.status === 403) {
+        console.log("API LIMIT", response.status);
+    }
+});
+const fetchPosts = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield getAxiosWithToken("https://api.github.com/repos/dnrgus1127/TIL/git/trees/main?recursive=10");
+    const tree = response.data.tree;
+    return tree.filter((node) => node.type === "blob");
+});
+const postTimeUpdate = (postList) => __awaiter(void 0, void 0, void 0, function* () {
+    const postPathList = postList.map((post) => post.path);
+    const getAxiosList = postPathList.map((path) => getAxiosWithToken(`https://api.github.com/repos/dnrgus1127/TIL/commits?path=${path}&per_page=1&page=1`));
+    return axios.all(getAxiosList).then(axios.spread((...rest) => {
+        const postTimes = rest.map((item) => item.data[0].commit.committer.date);
+        return postList.map((post, idx) => {
+            return Object.assign(Object.assign({}, post), { timeStamp: postTimes[idx] });
+        });
+    }));
+});
+const getProjectTree = (projectName) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield getAxiosWithToken(`https://api.github.com/repos/dnrgus1127/${projectName}/git/trees/main?recursive=1`);
+    return response.data;
+});
+const getPost = (title) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield getAxiosWithToken(`https://raw.githubusercontent.com/dnrgus1127/TIL/main/${encodeURIComponent(title)}`);
+    return response.data;
+});
+module.exports = { fetchData, fetchPosts, postTimeUpdate, getProjectTree, getPost };
